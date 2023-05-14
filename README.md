df676e8d582ab1be5183c05688a70b4e5c1bbff5
shangeeran <shangeeran@gmail.com>
2023-05-15 01:07:05 +0530

    finalized

---
+++ README.md
@@ -0,0 +1,14 @@
+Welcome to EMERALD!
+
+
+This is the hotel website home page, each section is developed besed on the stars hotel sites
+
+It's developed for interview task
+
+Here is the one HTML includes all the contents
+
+Unzip the file and just click the index.html file, its work!
+
+All the images are stored under the "img" folder.
+
+All the explanations are between the code lines for more understanding.
---
+++ js/script.js
@@ -0,0 +1,39 @@
+// its for destination Selection
+
+$(document).ready(function(){
+    $("select").change(function(){
+        $(this).find("option:selected").each(function(){
+            var optionValue = $(this).attr("value");
+            if(optionValue){
+                $(".box").not("." + optionValue).hide();
+                $("." + optionValue).show();
+            } else{
+                $(".box").hide();
+            }
+        });
+    }).change();
+});
+
+let slideIndex = 0;
+showSlides();
+
+
+
+// its for slider Selection
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
